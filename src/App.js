4b2dc15d92ece04d87c7f3ce2299e3b9239e6d18
이기준 <kijun@igijun-ui-MacBookPro.local>
2022-04-07 15:14:31 +0900

    드라마 목록 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Drama({name, poster, rating}) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <img src={poster} />
+      <h2>Rating: {rating}</h2>
+    </div>
+  )
+}
+
+const dramaILike = [
+  {
+    name: '시그널',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.-Kzt3xbxLcGJEl9j-HlJDQHaKt%26pid%3DApi&f=1',
+    rating: 4.8
+  },
+  {
+    name: '도깨비',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F2505D649584F80AF11&f=1&nofb=1',
+    rating: 4.5
+  },
+  {
+    name: '구해줘',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.ytn.co.kr%2Fosen%2F2017%2F07%2F20170714_1500001603_81590000_1.jpg&f=1&nofb=1',
+    rating: 4.5
+  },
+  {
+    name: '스토브리그',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffile.mk.co.kr%2Fmeet%2Fneds%2F2019%2F12%2Fimage_readtop_2019_1011282_15753700184000446.jpg&f=1&nofb=1',
+    rating: 4.9
+  },
+  {
+    name: '응답하라 1994',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.themoviedb.org%2Ft%2Fp%2Foriginal%2FhgTgCEkfxPQyXiKVszHomwazm7E.jpg&f=1&nofb=1',
+    rating: 4.8
+  }
+]
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {dramaILike.map(drama=> <Drama key={drama.name} name={drama.name} poster={drama.image} rating={drama.rating} />)}
     </div>
   );
 }
